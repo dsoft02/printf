635826aba7c45cdd64f3cd36a3d3a4aba74defc4
Ebenezer Ogidiolu <ogidioluebenezer@gmail.com>
2022-10-18 06:04:09 -0700

    my first commit on 0x11. C - printf

---
+++ README.md
@@ -0,0 +1,4 @@
+# 0x011 .C Printf
+This is a team project that is aimed at understanding the concept of team work by replicating the printf function of the standard c library
+
+---
---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdlib.h>
+#include <stdarg.h>
+
+/* _printf */
+int _printf(const char *format, ...);
+
+/* write_funcs */
+int _putchar(char c);
+int _puts(char *str);
+
+#endif
